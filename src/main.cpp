f7881ed66897a9e923061270b35c0ce802a05a03
dhruvg <aruna.dhruv.g@gmail.com>
2025-06-11 14:28:47 -0400

    gg

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <filesystem>
+#include <csdlib>
 
 std::string getSubString(int index, std::string input, int length){
   std::string phrase{input.substr(index, length)};
   return phrase;
 }
-int main() {
+int checkDirForFile(std::string directory, std::string command){
+  for (const auto & entry : std::filesystem::directory_iterator(directory)) {
+    std::string path{entry.path()};
+    if( path.contains(command) ){
+      std::cout << command << " is " << directory;
+      return 1;
+    }
+  }
+  return 0;
+}
+std::vector<std::string> getPaths(std::string argPath){
+  bool pathSearch = true;
+  std::vector<std::string> paths;
+
+  int startIndex{0};
+  while (pathSearch){
+    int index = argPath.find(':');
+    if (index = -1){
+      pathSearch = false;
+      break;
+    }
+    paths.push_back(getSubString(0,argPath,index));
+    argPath = getSubString(index,argPath,(argPath.length() - index));
+  }
+
+  return paths;
+}
+
+int main(int argc, char* argv[]) {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  std::vector<std::string> paths = getPaths(system("echo $PATH"));
 
   // Uncomment this block to pass the first stage
   while (true){
@@ -21,16 +54,17 @@ int main() {
       }
     }
     else if (getSubString(0, input, 4) == "echo"){
-      int echoIndex{input.find(' ') + 1}; //Returns the index of the first character after echo
+      long unsigned int echoIndex{input.find(' ') + 1}; //Returns the index of the first character after echo
       std::string phrase = getSubString(echoIndex,input,(input.length() - echoIndex));
       std::cout << phrase << '\n';
     }
     else if (getSubString(0, input, 4) == "type"){
-      int typeIndex{input.find(' ') + 1}; //Returns the index of the first character after type
+      long unsigned int typeIndex{input.find(' ') + 1}; //Returns the index of the first character after type
       std::string command = getSubString(typeIndex, input, (input.length() - typeIndex));
       if (command.contains("echo")){
         std::cout << "echo is a shell builtin" << '\n';
       }
+      
       else if (command.contains("exit")){
         std::cout << "exit is a shell builtin" << '\n';
       }
@@ -38,7 +72,16 @@ int main() {
         std::cout << "type is a shell builtin" << '\n';
       }
       else{
-        std::cout << command << ": not found" << '\n';
+        int commandStatus{};
+        for (int i = 0; i < paths.size(); i++){
+          commandStatus = checkDirForFile(paths[i],command);
+          if(commandStatus){
+            break;
+          }
+        }
+        if (commandStatus == 0){
+          std::cout << command << ": not found" << '\n';
+        }
       }
     }
     else {
@@ -47,3 +90,4 @@ int main() {
   }
   
 }
+
